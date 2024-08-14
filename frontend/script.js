5964538fd4c3b808d95edff580338f79539d92cb
Tommaso Cerruti <tommaso.cerruti@casavo.com>
2024-08-14 18:44:38 +0200

    feat: script to calculate the result

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,28 @@
-// script still to be implemented
+function calculate() {
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert('Please enter non null valid numbers');
+        return;
+    }
+
+    fetch('http://127.0.0.1:5000/calculate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ num1: num1, num2: num2 }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error) {
+            alert(data.error);
+        } else {
+            document.getElementById('result').innerText = 'Result: ' + data.result;
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
