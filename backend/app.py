a20f398775904e353fefcf7897c3363f2e99eee8
Tommaso Cerruti <tommaso.cerruti@casavo.com>
2024-08-14 22:26:39 +0200

    chore: formatted app.py using black

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,26 @@
 from flask import Flask, request, jsonify, send_from_directory
 from sympy import nextprime
 
-app = Flask(__name__, static_folder='../frontend')
+app = Flask(__name__, static_folder="../frontend")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory(app.static_folder, "index.html")
 
-@app.route('/calculate', methods=['POST'])
+
+@app.route("/calculate", methods=["POST"])
 def calculate():
     try:
         data = request.get_json()
-        num1 = int(data.get('num1'))
-        num2 = int(data.get('num2'))
+        num1 = int(data.get("num1"))
+        num2 = int(data.get("num2"))
         result = operation(num1, num2)
-        return jsonify({'result': result})
+        return jsonify({"result": result})
 
     except Exception as e:
-        return jsonify({'error': str(e)}), 400
+        return jsonify({"error": str(e)}), 400
+
 
 def operation(num1, num2):
     total_sum = num1 + num2
@@ -25,5 +28,6 @@ def operation(num1, num2):
     prime = nextprime(max_num)
     return total_sum + prime
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
